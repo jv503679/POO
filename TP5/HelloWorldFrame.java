88bfc4519dfd604f6d18ea682b6dfbc4e8cc5ace
jv503679 <victor.jung.06.d3@gmail.com>
2017-10-03 17:12:05 +0200

    Update HelloWorldFrame.java

--- TP5/HelloWorldFrame.java
+++ TP5/HelloWorldFrame.java
@@ -2,8 +2,8 @@ import javax.swing.JFrame;
 
 public class HelloWorldFrame {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame(); // creation d’une fenetre graphique
-		frame.setSize(800, 600); // taille de la fenetre
+		JFrame frame = new JFrame(); // creation dâ€™une fenetre graphique
+		frame.setSize(800, 160); // taille de la fenetre
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		// comportement lors de la fermeture
 		frame.setLocationRelativeTo(null); // on centre la fenetre
