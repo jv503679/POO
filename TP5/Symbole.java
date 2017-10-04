7c706ca13bafdd39da68b51395ff699e8fc925eb
jv503679 <victor.jung.06.d3@gmail.com>
2017-10-04 16:13:42 +0200

    Update Symbole.java

--- TP5/Symbole.java
+++ TP5/Symbole.java
@@ -4,9 +4,9 @@ public class Symbole {
 	private Epaisseur e;
 	private Nature n;
 	
-	public Symbole(int e, int n){
-		if(e == 0){ this.e = Epaisseur.ETROIT; } else { this.e = Epaisseur.LARGE ; }
-		if(n == 0){ this.n = Nature.BARRE; } else { this.n = Nature.ESPACE ; }
+	public Symbole(Epaisseur e, Nature n){
+		this.e = e;
+		this.n = n;
 	}
 	
 	public void draw(Graphics g, int x, int y){
