cdb834116d578d06097b2b3aca0a5fc8350f4bab
jv503679 <victor.jung.06.d3@gmail.com>
2017-10-04 16:13:17 +0200

    Update IterateurSymbole.java

--- TP5/IterateurSymbole.java
+++ TP5/IterateurSymbole.java
@@ -1,30 +1,23 @@
 public class IterateurSymbole {
-	private String s;
+	private final String s;
 	private int acc, len;
-	private boolean state;
 	
 	public IterateurSymbole(String s){
 		this.s = s;
 		this.len = s.length();
 		acc = 0;
-		state = true;
-	}
-	
-	private void changeState(){
-		state = !state;
-	}
-	
-	public int getState(){
-		if(state){ return 1; } else { return 0 ; }
 	}
 	
 	public boolean hasNext(){
-		return (acc) < len;
+		return acc < len;
 	}
 	
 	public char next(){
-		acc = acc + 1;
-		changeState();
-		return s.charAt(acc-1);
+		acc += 1;
+		return s.charAt(acc - 1);
+	}
+	
+	public int getAcc(){
+		return acc;
 	}
 }
