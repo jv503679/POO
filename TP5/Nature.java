7c3123f69a57a74df8742652756056a389bb00ec
jv503679 <victor.jung.06.d3@gmail.com>
2017-10-10 22:26:58 +0200

    Update Nature.java

--- TP5/Nature.java
+++ TP5/Nature.java
@@ -5,6 +5,7 @@ public enum Nature {
 	BARRE(new Color(0,0,0)),
 	ESPACE(new Color(255,255,255));
 	
+	private static final Nature[] tab = {Nature.ESPACE, Nature.BARRE};
 	private final Color c;
 	
 	private Nature(Color c){
