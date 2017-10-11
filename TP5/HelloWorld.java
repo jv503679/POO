4168e7111db7236d11d186973bea3a526ebdcf48
jv503679 <victor.jung.06.d3@gmail.com>
2017-10-11 10:11:33 +0200

    Update HelloWorld.java

--- TP5/HelloWorld.java
+++ TP5/HelloWorld.java
@@ -5,12 +5,9 @@ class HelloWorld extends JPanel {
 	
 	private void drawBarCode(Graphics g, String code, int x, int y){
 		IterateurSymbole i = new IterateurSymbole(Code39.code(code));
-		int e, n;
 		Symbole s;
 		while(i.hasNext()){
-			e = Character.getNumericValue(i.next());
-			n = i.getAcc() % 2;
-			s = new Symbole(Epaisseur.tab[e], Nature.tab[n]);
+			s = i.next();
 			s.draw(g, x, y);
 			x += s.getWidth();
 		}
